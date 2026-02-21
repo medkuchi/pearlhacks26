e5ad9487bb048ab7a5fef4b1da1478fb8a4d3555
Siyu Liu <siyuliu@unc.edu>
2026-02-21 12:06:52 -0500

    localhost working

---
+++ app.py
@@ -0,0 +1,18 @@
+# Imports
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+# My App
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def index():
+    return "Testing 123"
+
+
+
+if __name__ in "__main__":
+    app.run(debug=True)
