878d51ed264d1faa298de15cbcd5cdb5a12edd9d
Siyu Liu <siyuliu@unc.edu>
2026-02-21 15:44:36 -0500

    Footer+header+app

--- app.py
+++ app.py
@@ -12,7 +12,20 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+#change this to ("/")
+@app.route("/Swipe")
+def swipe():
+    return render_template("swipe.html")
+
+@app.route("/Messages")
+def chat():
+    return render_template("chat.html")
+
+@app.route("/Notifications")
+def matches():
+    return render_template("matches.html")
 
 
-if __name__ in "__main__":
+
+if __name__ == "__main__":
     app.run(debug=True)
